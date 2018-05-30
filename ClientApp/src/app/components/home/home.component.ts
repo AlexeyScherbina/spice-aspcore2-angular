57fdbd1b2631fd8f14a25051ee685c5511b31757
AlexeyScherbina <scherbina.lyosha@yandex.ru>
2018-05-30 20:55:06 +0300

    sideBar changes

--- ClientApp/src/app/components/home/home.component.ts
+++ ClientApp/src/app/components/home/home.component.ts
@@ -8,29 +8,56 @@ import { HttpClient } from '@angular/common/http';
 export class HomeComponent {
   public categories: Category[];
 
+  
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     http.get<Category[]>(baseUrl + 'api/Category/getHierarchy').subscribe(result => {
       this.categories = result;
-      console.log(result);
+      this.disActivateAll();
       console.log(this.categories);
     }, error => console.error(error));
   }
+
+  public disActivateAll() {
+    this.categories.forEach(function (cat) {
+      cat.active = false;
+      cat.Process.forEach(function (proc) {
+        proc.active = false;
+      })
+    });
+  }
+
+  public checkActive(cat: Category) {
+    if (cat.active == true) {
+      return true;
+    }
+    for (let i = 0; i < cat.Process.length; i++) {
+      if (cat.Process[i].active == true) {
+        return true;
+      }
+
+    }
+    
+    return false;
+  }
+  
 }
 
 interface Category {
-  CategoryID: number;
+  CategoryId: number;
   Title: string;
   Description: string;
   Process: Process[];
+  active: boolean;
 }
 interface Process {
-  ProcessID: number;
+  ProcessId: number;
   Title: string;
   Description: string;
   Practice: Practice[];
+  active: boolean;
 }
 interface Practice {
-  PracticeID: number;
+  PracticeId: number;
   Title: string;
   Description: string;
 }
